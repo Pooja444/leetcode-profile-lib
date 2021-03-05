cdb438b1c2e53ca98c1abf41061a6f844d3daa62
Pooja Kulkarni <pakcutie14@gmail.com>
2021-03-05 22:45:30 +0530

    Added express server to support data retrieval from backend

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import { apolloClient } from './services/apollo.service';
 import { AllQuestionsCount } from './models/allQuestionsCount';
 import { getAllQuestionsCountQuery } from './graphql/questions.graphql';
 
+import express from 'express';
+
 export async function getAllQuestionsCount(): Promise<AllQuestionsCount> {
     const response = await apolloClient.query<AllQuestionsCount>({
         query: getAllQuestionsCountQuery()
@@ -18,3 +20,13 @@ export async function getUserProfile(username: string): Promise<MatchedUser> {
     })
     return response.data
 }
+
+const app = express()
+
+app.use('/leetprofile/:user', async (req, res) => {
+    res.send((await getUserProfile(req.params.user)).matchedUser)
+})
+
+app.listen(1100, '0.0.0.0', () => {
+    console.log('Server listening on:', 'http://0.0.0.0:1100');
+});
