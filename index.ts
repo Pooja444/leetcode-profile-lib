a6337aa2cdfc6265e3dd119125eb3c70c5ce6234
“Pooja <pakcutie14@gmail.com>
2022-12-21 16:28:31 +0530

    Removed express code

--- index.ts
+++ index.ts
@@ -92,31 +92,3 @@ export async function getLeetQuestionsCount(req: any, res: { send: (arg0: Questi
         res.send(questionsResponse)
     }
 }
-
-
-// Delete below code later - and add it to leetprofile.server
-
-const app = express()
-
-app.use(cors())
-app.get('/user/:username/badges', getUserBadges)
-app.get('/user/:username/calendar', getUserCalendarWithoutYear)
-app.get('/user/:username/calendar/:year', getUserCalendarWithYear)
-app.get('/user/:username/community-stats', getUserCommunityStats)
-app.get('/user/:username/contest-ranking-history', getUserContestRankingHistory)
-app.get('/user/:username/contest-ranking', getUserContestRanking)
-app.get('/user/:username/discussions', getUserDiscussions)
-app.get('/user/:username/languages', getUserLanguages)
-app.get('/user/:username/problems-solved-beats-stats', getUserProblemsSolvedBeatsStats)
-app.get('/user/:username/profile', getUserProfile)
-app.get('/user/:username/recent-submissions', getUserRecentSubmissions)
-app.get('/user/:username/skills', getUserSkills)
-app.get('/user/:username/social', getUserSocial)
-app.get('/user/:username/solutions', getUserSolutions)
-app.get('/user/:username/submit-stats', getUserSubmitStats)
-
-const port = 3200
-
-app.listen(port, () => {
-    console.log('Listening on server https://localhost:3200/')
-})
